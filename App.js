b9631434aca10079b70667f6e063ab6587f2a562
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-10-20 12:03:45 -0400

    create button to update text using useState hooks

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar} from 'expo-status-bar';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
-export default function App() {
+export default function App() { 
+
+  const [outputText, setOutputText] = useState("Ian's Raact Native App")
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText} </Text>
+      <Button title="Press Button" onPress={()=> setOutputText("New Text")}/>
+      <StatusBar style="auto" /> 
     </View>
   );
 }
