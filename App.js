2de105cc61ce52e3b8356cc369cdd14eaa1769c1
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-06 16:51:47 -0400

    code clean uo

--- App.js
+++ App.js
@@ -6,42 +6,25 @@ import GoalsInput from './components/GoalsInput';
 
 export default function App() {
 
-  // const [InputGoal, setInputGoal] = useState(" ");
-   const [goals, setGoals] = useState([])
+  const [goals, setGoals] = useState([])
 
-  // const goalInputHandler = (enteredText) => {
-  //   setInputGoal(enteredText);
-  // };
 
   const onPressHandler = (InputTitle) => {
     // setGoals([...goal, InputGoal]) 
     setGoals(currentState => [...currentState,
-    { key: Math.random().toString(), value: InputTitle}
+    { key: Math.random().toString(), value: InputTitle }
     ])
   }
 
 
   return (
     <View style={styles.container}>
-      {/* <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Add Text Here"
-          style={styles.textInput}
-          onChangeText={goalInputHandler}
-          value={InputGoal}
-        />
-        <Button
-          title="Add"
-          onPress={onPressHandler} />
-      </View> */} 
-      <GoalsInput onPressFunc={onPressHandler}/>
-
+      <GoalsInput onPressFunc={onPressHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={goals}
         renderItem={itemData => <Goals title={itemData.item.value} />}
       />
-
     </View>
   );
 }
--- components/GoalsInput.js
+++ components/GoalsInput.js
@@ -1,47 +1,45 @@
-import React, {useState} from 'react'; 
-import {StyleSheet, View, TextInput, Button } from 'react-native'; 
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
-const GoalsInput = (props) => { 
+const GoalsInput = (props) => {
 
     const [InputGoal, setInputGoal] = useState(" ");
-    
-  
+
+
     const goalInputHandler = (enteredText) => {
-      setInputGoal(enteredText);
+        setInputGoal(enteredText);
     };
-  
 
-    return(
+
+    return (
         <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Add Text Here"
-          style={styles.textInput}
-          onChangeText={goalInputHandler}
-          value={props.InputGoal}
-        />
-        <Button
-          title="Add"
-          onPress={props.onPressFunc.bind(this, InputGoal)} />
-      </View>
-
-
+            <TextInput
+                placeholder="Add Text Here"
+                style={styles.textInput}
+                onChangeText={goalInputHandler}
+                value={props.InputGoal}
+            />
+            <Button
+                title="Add"
+                onPress={props.onPressFunc.bind(this, InputGoal)} />
+        </View>
     );
-}; 
+};
 
-export default GoalsInput  
+export default GoalsInput
 
 const styles = StyleSheet.create({
     inputContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center'
-      },
-      textInput: {
+    },
+    textInput: {
         width: '80%',
         borderColor: 'black',
         borderWidth: 1,
         padding: 10
-      },
+    },
 
 })
 
